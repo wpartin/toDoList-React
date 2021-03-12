30015e30d829d3bc06ea58674e13f0fd3285c83a
Weston Partin <djwest91@gmail.com>
2021-03-12 08:32:41 -0600

    added react-icons, tasks & task.js updates

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import Button from "./Button";
 const Header = ({ title }) => {
+  const onClick = (e) => {
+    console.log(e);
+  };
+
   return (
-    <header>
+    <header className="header">
       <h1>{title}</h1>
+      <Button color="green" text="Add" onClick={onClick} />
     </header>
   );
 };
